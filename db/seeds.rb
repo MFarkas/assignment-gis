54d5f2ff2206fbea9defc6b55e151dce89f7d4ee
MFarkas <michalfarkas1@gmail.com>
2015-11-26 17:43:14 +0100

    update to presented version

--- db/seeds.rb
+++ db/seeds.rb
@@ -166,20 +166,20 @@ sql= "CREATE INDEX tsv_GIN ON conflicts USING gin(tsv);"
 ActiveRecord::Base.connection.execute(sql)
 ###########INDEXES############################
 
-sql= "CREATE UNIQUE INDEX fatalities_idx ON conflicts (fatalities);"
+sql= "CREATE INDEX fatalities_idx ON conflicts (fatalities);"
 ActiveRecord::Base.connection.execute(sql)
 
-sql= "CREATE UNIQUE INDEX event_date_idx ON conflicts (event_date);"
+sql= "CREATE INDEX event_date_idx ON conflicts (event_date);"
 ActiveRecord::Base.connection.execute(sql)
 
-sql= "CREATE UNIQUE INDEX country_id_idx ON conflicts (country_id);"
+sql= "CREATE INDEX country_id_idx ON conflicts (country_id);"
 ActiveRecord::Base.connection.execute(sql)
 
-sql= "CREATE UNIQUE INDEX event_type_idx ON conflicts (event_type);"
+sql= "CREATE INDEX event_type_idx ON conflicts (event_type);"
 ActiveRecord::Base.connection.execute(sql)
 
-sql= "CREATE UNIQUE INDEX inter1_idx ON conflicts (inter1);"
+sql= "CREATE INDEX inter1_idx ON conflicts (inter1);"
 ActiveRecord::Base.connection.execute(sql)
 
-sql= "CREATE UNIQUE INDEX inter2_idx ON conflicts (inter2);"
+sql= "CREATE INDEX inter2_idx ON conflicts (inter2);"
 ActiveRecord::Base.connection.execute(sql)
